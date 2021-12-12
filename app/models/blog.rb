35a022e5ad0ccebcf8f9cef3f5216c81d24603ed
JorgeAndaur <aenimalabs@gmail.com>
2021-12-11 21:59:34 -0300

    Blog.all.sample

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,7 +4,25 @@ class Blog < ApplicationRecord
     
     has_many :usuarios, :through => :propietarios, dependent: :destroy
 
-    validates :nombre, :descripcion, presence: true
+    validates :nombre, :descripcion, presence: true, uniqueness: { case_sensitive: false}
+
+    after_create :creacion_exitosa
+  
+  after_update :actualizacion_exitosa
+
+    
+
+  private
+ 
+  
+  def creacion_exitosa
+      puts "Blog creado con éxito"
+  end
+  
+  def after_update
+      puts "Blog actualizado con éxito"
+  end
+  
     
     
 
